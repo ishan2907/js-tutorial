d03f7412edd0cc5cf411db1d4ad749a1c5282bab
Ishan Baghel <104764689+ishan2907@users.noreply.github.com>
2024-12-09 13:08:13 +0000

    done

---
+++ 01_Basics/comparison.js
@@ -0,0 +1,6 @@
+console.log('2'>0)
+console.log(null>0)
+console.log(null==0)//should avoid such comparisons
+console.log(null >=0)
+//strict check ===
+console.log(2===2)
--- 01_Basics/datatype_conversion.js
+++ 01_Basics/datatype_conversion.js
@@ -1,4 +1,4 @@
-let score=33//this is a number
+/*let score=33//this is a number
 let scr="33"//this is string
 
 
@@ -6,4 +6,14 @@ console.log(typeof score)
 let valueInNumber=Number(scr)//data type conversion from string to integer
 console.log(valueInNumber)
 /*but perform data type conversion carefully you might encounter lots of problem */
-//true gets converted to 1 and false gets to 0
+//true gets converted to 1 and false gets to 0 */
+
+/* Operations */
+let value=3
+let negValue=-value
+//console.log(negValue)
+//console.log(2**2)//two to the power of two
+let str1="hii"
+let str2=" ishan"
+let str3=str1+str2
+console.log(str3)
