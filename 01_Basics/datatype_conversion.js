cbe758410a49b9a585c1268f7d9d6c30b9bdc0bf
Ishan Baghel <104764689+ishan2907@users.noreply.github.com>
2024-12-09 11:07:40 +0000

    conversion done

---
+++ 01_Basics/datatype_conversion.js
@@ -0,0 +1,9 @@
+let score=33//this is a number
+let scr="33"//this is string
+
+
+console.log(typeof score)
+let valueInNumber=Number(scr)//data type conversion from string to integer
+console.log(valueInNumber)
+/*but perform data type conversion carefully you might encounter lots of problem */
+//true gets converted to 1 and false gets to 0
