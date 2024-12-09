858477bcd7a71d46dbd4003a64671fd320cc80a2
Ishan Baghel <104764689+ishan2907@users.noreply.github.com>
2024-12-09 14:24:39 +0000

    done

---
+++ 01_Basics/string.js
@@ -0,0 +1,22 @@
+const name="isha-n"
+const gameName=String("isha-n-baghel-mohan")
+/*const repoCount=50;
+console.log(name+ repoCount +"HII");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+const gameName=new String("ishan")
+console.log(gameName.__proto__)
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase())*/
+const newString=gameName.substring(0,3)
+console.log(newString)
+const anotherString=gameName.slice(0,4)
+console.log(anotherString)
+const newString1="    ishan    "
+console.log(newString1.trim())
+const url="https://ishan.com?ishan baghel"
+/*url.replace('ishan','aashu');
+console.log(url)*/
+console.log(url.includes('ishan'))
+
+console.log(gameName.split('-')) //splitting string into array on the basis of -
