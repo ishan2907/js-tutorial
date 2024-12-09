c3e33c81b1d535ecda52eac8536a62fd0da86ab4
Ishan Baghel <104764689+ishan2907@users.noreply.github.com>
2024-12-09 10:48:34 +0000

    done

---
+++ 01_Basics/datatype.js
@@ -0,0 +1,18 @@
+// alert("hello")
+// cant use we are using node js not browser
+let name="Ishan"
+let age=21
+let isLoggedIn=true
+/*primitive */
+//number => 2 to power 53
+//bigint
+//string-can use both single and double quotes
+//boolean=true or false
+//null-stanalone value (empty value)
+//undefined=>havent defined value 
+//symbol-to represent uniqueness
+//null is an object but undefined is undefined
+
+//undefined is a type in js
+/*objects */
+console.log(typeof "ishan");
---
+++ 01_Basics/variables.js
@@ -0,0 +1,10 @@
+const accountId=144553
+let accountEmail="ishanbaghel.com"
+var accountPassword="12345"
+accountCity="Seoni"
+let accountState;
+
+// accountId=2;//not allowed
+//do not use var otherwise you will experience scope problem
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword])
