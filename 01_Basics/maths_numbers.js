6f601d4d0c00bd1624303f008bf626586ea7ee45
Ishan Baghel <104764689+ishan2907@users.noreply.github.com>
2024-12-10 05:01:53 +0000

    done

---
+++ 01_Basics/maths_numbers.js
@@ -0,0 +1,30 @@
+// const score=400
+
+// const balance =new Number(100);//explicitly defining the data type
+// console.log(balance)
+
+// console.log(balance.toString().length);
+// const otherNumber=23.8966
+// console.log(otherNumber.toPrecision(3))
+
+// const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+/* maths */
+console.log(Math);//this is an object
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.7));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9))
+console.log(Math.min(43,23,12));//min value in an array,max func is there too
+console.log(Math.random());//generated random value between 0 and 1
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
+
