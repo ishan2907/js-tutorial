2e3fc1836c919727b4620731fdce7c57e2785684
Ishan Baghel <104764689+ishan2907@users.noreply.github.com>
2024-12-13 09:40:18 +0000

    done

---
+++ 02_Basics/newScope.js
@@ -0,0 +1,24 @@
+// function one(){
+//     let username="ishan"
+
+//     function two(){
+//         let website="tinder"
+//         console.log(username)
+//     }
+//     // console.log(website)
+
+//     two()
+// }
+// one()
+
+//***** interesting concept** */
+function addOne(value){
+    return value+1
+}
+console.log(addOne(5))
+
+
+const addTwo=function(num){
+    return num+2;
+}
+console.log(addTwo(2))
